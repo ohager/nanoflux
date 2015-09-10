d2ecc02d7c754261740ec6bf39642ca59bc88173
ohager <oliver.hager@dextra.com.br>
2015-09-09 23:38:11 -0300

    More tests

--- spec/dispatcher-spec.js
+++ spec/dispatcher-spec.js
@@ -1,26 +1,34 @@
 var dispatcherFactory = require("../src/dispatcher");
+var storeFactory = require("../src/store");
 
-describe("dispatcher", function () {
+describe("Dispatcher Creation", function () {
 
     it("should create multiple actions 'action1' and 'action2", function () {
         var dispatcher = dispatcherFactory.create('myDispatcher', ['action1','action2']);
-        expect(dispatcher.action1).not.toBe(undefined);
-        expect(dispatcher.action2).not.toBe(undefined);
+        expect(dispatcher.action1).toBeDefined();
+        expect(dispatcher.action2).toBeDefined();
         expect(typeof(dispatcher.action1)).toBe("function");
         expect(typeof(dispatcher.action2)).toBe("function");
     });
 
     it("should create a single actions 'action1'", function () {
         var dispatcher = dispatcherFactory.create('myDispatcher1', 'action1');
-        expect(dispatcher.action1).not.toBe(undefined);
+        expect(dispatcher.action1).toBeDefined();
         expect(typeof(dispatcher.action1)).toBe("function");
     });
 
     it("should return the created dispatcher 'myDispatcher'", function () {
         dispatcherFactory.create('myDispatcher1', 'action1');
         var dispatcher = dispatcherFactory.getDispatcher('myDispatcher1');
-        expect(dispatcher).not.toBe(undefined);
+        expect(dispatcher).toBeDefined();
         expect(typeof(dispatcher.action1)).toBe("function");
     });
+
+
+    it("can create without any action", function () {
+        var dispatcher = dispatcherFactory.create('myDispatcher1');
+        expect(dispatcher).toBeDefined();
+    });
+
 });
 
--- spec/nanoflux-spec.js
+++ spec/nanoflux-spec.js
@@ -1,6 +1,6 @@
 var NanoFlux = require("../src/nanoflux");
 
-describe("NanoFlux", function () {
+describe("NanoFlux Basics", function () {
 
     it("should create store 'myStore", function () {
         var store = NanoFlux.createStore('myStore', {
@@ -26,4 +26,80 @@ describe("NanoFlux", function () {
     });
 
 });
+
+
+describe("NanoFlux Dispatching", function () {
+
+    var result = null;
+    var store = NanoFlux.createStore('myStore', {
+        onAction1 : function(data){
+            result = data;
+        },
+        onAction2 : function(data){
+            result = data * 2;
+        }
+    });
+
+    function ActionProvider(dispatcher){
+        this.action1 = function(data){
+            dispatcher.dispatch('action1',data);
+        };
+
+        this.action2 = function(data){
+            dispatcher.dispatch('action2',data);
+        };
+
+    }
+
+    it("should dispatch 'static' actions 'action1' and 'action2 (Fluxy)", function () {
+        var dispatcher = NanoFlux.createDispatcher('myDispatcher', ['action1','action2']);
+        store.connectTo(dispatcher);
+
+        dispatcher.action1("Action1");
+        expect(result).toBe("Action1");
+
+        dispatcher.action1("Action1.1");
+        expect(result).toBe("Action1.1");
+
+        dispatcher.action2(2);
+        expect(result).toBe(4);
+    });
+
+    it("should dispatch 'dynamic' actions 'action1' and 'action2 (Full Flux)", function () {
+        var dispatcher = NanoFlux.createDispatcher('myDispatcher');
+        store.connectTo(dispatcher);
+        var actions = new ActionProvider(dispatcher);
+
+        actions.action1("Action1");
+        expect(result).toBe("Action1");
+
+        actions.action1("Action1.1");
+        expect(result).toBe("Action1.1");
+
+        actions.action2(2);
+        expect(result).toBe(4);
+    });
+
+
+    it("should be able to use 'static' and  'dynamic' actions 'action1' and 'action2", function () {
+        var dispatcher = NanoFlux.createDispatcher('myDispatcher', 'action1');
+        store.connectTo(dispatcher);
+        var actions = new ActionProvider(dispatcher);
+
+        dispatcher.action1("Action1");
+        expect(result).toBe("Action1");
+
+        actions.action1("Action1.1");
+        expect(result).toBe("Action1.1");
+
+        actions.action1("Action1.2");
+        expect(result).toBe("Action1.2");
+
+        actions.action2(2);
+        expect(result).toBe(4);
+    });
+
+
+});
+
 
