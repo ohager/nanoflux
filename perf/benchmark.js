fd0bb32e438b09ed4779add58c6fd2d2fce9a347
ohager <oliver.hager@dextra.com.br>
2015-09-11 18:28:38 -0300

    Performance Statistics

--- perf/benchmark.js
+++ perf/benchmark.js
@@ -10,10 +10,28 @@ function run(f){
     console.log(r.name + ': ' + r.ops  + ' op/s, avg. ' + r.avg + ' ms per operation');
 }
 
+function analyseResults(){
+
+    console.log('\nResult:\n-------------------------------\n');
+
+    results.sort( function(a,b){
+        return b.ops - a.ops;
+    });
+    var fastest = results[0];
+
+    for(var i = 0; i < results.length; ++i){
+        var r = results[i];
+        var percent = (r.ops / fastest.ops) * 100.00;
+
+        console.log( i+1 + '. ' + r.name + ': ' + r.ops + ' op/s (' + (r.ops - fastest.ops).toFixed(2) + ' op/s) - ' + percent.toFixed(2) + '%' );
+    }
+}
+
 run(fluxyPerf);
 run(fullfluxPerf);
 run(refluxPerf);
 
+analyseResults();
 
 
 
